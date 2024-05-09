34440faa9ce7f1b860febbf5ac8d8b582ca8096f
Given Omondi <omondigiven@gmail.com>
2024-05-09 21:07:12 +0300

    created headed component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Home from "./Pages/Home";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
-import SignIn from "./Pages/SignIn";
+// import SignIn from "./Pages/SignIn";
+import Header from "./Components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
+          {/* <Route path="/sign-in" element={<SignIn />} /> */}
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
