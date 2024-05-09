271e6daf87e52096770acca95e3421b62f39f102
Given Omondi <omondigiven@gmail.com>
2024-05-09 19:15:52 +0300

    fixed not displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import Home from "./Pages/Home"
-import SignUp from "./Pages/SignUp"
-import About from "./Pages/About"
-import Profile from "./Pages/Profile"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={Home}/>
-          <Route />
-          <Route path="/sign-up" element={SignUp}/>
-          <Route path="/About" element={About}/>
-          <Route path="/Profile" element={Profile}/>
-          
-
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
