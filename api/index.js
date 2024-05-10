38399292efd1bfaff8bbf1b82c2cedbd5b2c623e
Given Omondi <omondigiven@gmail.com>
2024-05-11 00:00:18 +0300

    fixed server error and created signup module

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
-
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_CONNECT).then(() => {
@@ -13,27 +12,21 @@ mongoose.connect(process.env.MONGO_CONNECT).then(() => {
 });
 
 const app = express();
-
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000 and hello!!");
 });
 
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+    const message = err.message || 'Internal Server error';
     return res.status(statusCode).json({
-        success: false,
+        success:false,
         statusCode,
         message,
     });
 });
-
-
-
-
-
-
