673c3ff4581e0f985b2a183d1cd103f1efd7983f
Given Omondi <omondigiven@gmail.com>
2024-05-11 23:17:47 +0300

    fixed redux

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { SignInStart } from '../redux/user/userSlice';
+import { SignInSuccess } from '../redux/user/userSlice';
+import { SignInFailure } from '../redux/user/userSlice';
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -13,12 +19,12 @@ export default function SignIn() {
       [e.target.id]: e.target.value,
     });
   };
-  const navigate = useNavigate();
+  
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     try{
-      e.preventDefault();
-      setLoading(true);
+      dispatch(SignInStart());
       const res = await fetch('/api/auth/signin' , 
       {
         method: 'POST',
@@ -28,17 +34,15 @@ export default function SignIn() {
         body:JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log(data);
       if(data.success == false){
-        setError(data.message);
-        setLoading(false);
+        dispatch(SignInFailure(data.message));
         return;
       }
-        setLoading(false);
-        setError(null);
+        dispatch(SignInSuccess(data));
         navigate('/');
       }catch(error){
-        setLoading(false);
-        setError(error.message);
+       dispatch(SignInFailure(error.message));
     }
   };
 
@@ -61,13 +65,13 @@ export default function SignIn() {
           onChange={handleChange}
         />
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-         {loading ? 'Loading...' : 'Sign Up'}
+         {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont an account?</p>
         <Link to="/sign-up" className="text-blue-700">
-          Sign In
+          Sign Up
         </Link>
       </div>
       {error && <p className="text-red-500 mt-3">{error}</p>}
